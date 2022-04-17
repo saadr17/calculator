ffe016b54e5e0eaa27e8e33fdd74b7e2e576822a
Saad Raja <shraja7@gmail.com>
2022-04-17 07:02:18 -0500

    basic calculator functionalities

--- script.js
+++ script.js
@@ -1,70 +1,118 @@
-// class Calculator {
-//   constructor(previousOperandTextElement, currentOperandTextElement) {
-//     this.previousOperandTextElement = previousOperandTextElement;
-//     this.currentOperandTextElement = currentOperandTextElement;
-//     this.clear();
-//   }
-//   clear() {
-//     this.currentOperand = "";
-//     this.previousOperand = "";
-//     this.operation = undefined;
-//   }
+const calculator = {
+  displayValue: "0",
+  firstOperand: null,
+  waitingForSecondOperand: false,
+  operator: null,
+};
+function inputDigit(digit) {
+  const { displayValue, waitingForSecondOperand } = calculator;
 
-//   delete() {}
-//   appendNumber(number) {
-//     this.currentOperand = number;
-//   }
+  if (waitingForSecondOperand === true) {
+    calculator.displayValue = digit;
+    calculator.waitingForSecondOperand = false;
+  } else {
+    calculator.displayValue =
+      displayValue === "0" ? digit : displayValue + digit;
+  }
 
-//   chooseOperation(operation) {}
-//   computer() {}
-//   updateDisplay() {
-//     this.currentOperandTextElement.innerText = this.currentOperand;
-//   }
-// }
+  console.log(calculator);
+}
+function inputDecimal(dot) {
+  if (calculator.waitingForSecondOperand === true) {
+    calculator.displayValue = "0.";
 
-// const numberButtons = document.querySelectorAll("[data-number]");
-// const operationButtons = document.querySelectorAll("[data-operation]");
-// const equalsButton = document.querySelectorAll("[data-equals]");
-// const deleteButton = document.querySelectorAll("[data-delete]");
-// const allClearButton = document.querySelectorAll("[data-all-clear]");
-// const previousOperandTextElement = document.querySelectorAll(
-//   "[data-previous-operand]"
-// );
-// const currentOperandTextElement = document.querySelectorAll(
-//   "[data-current-operand]"
-// );
+    calculator.waitingForSecondOperand = false;
 
-// const calculator = new Calculator(
-//   previousOperandTextElement,
-//   currentOperandTextElement
-// );
+    return;
+  }
 
-// numberButtons.forEach((button) => {
-//   button.addEventListener("click", () => {
-//     calculator.appendNumber(button.innerText);
-//     calculator.updateDisplay();
-//   });
-// });
-// --------------------------------------------------------------------------------
-const equalsButton = document.querySelector("[data-equals]");
+  if (!calculator.displayValue.includes(dot)) {
+    calculator.displayValue += dot;
+  }
+}
 
-equalsButton.addEventListener("click", () => {
-  console.log("testing");
+function handleOperator(nextOperator) {
+  const { firstOperand, displayValue, operator } = calculator;
+  const inputValue = parseFloat(displayValue);
+
+  if (operator && calculator.waitingForSecondOperand) {
+    calculator.operator = nextOperator;
+
+    console.log(calculator);
+
+    return;
+  }
+
+  if (firstOperand == null && !isNaN(inputValue)) {
+    calculator.firstOperand = inputValue;
+  } else if (operator) {
+    const result = calculate(firstOperand, inputValue, operator);
+    calculator.displayValue = `${parseFloat(result.toFixed(7))}`;
+    calculator.firstOperand = result;
+  }
+
+  calculator.waitingForSecondOperand = true;
+  calculator.operator = nextOperator;
+  console.log(calculator);
+}
+
+function calculate(firstOperand, secondOperand, operator) {
+  if (operator === "+") {
+    return firstOperand + secondOperand;
+  } else if (operator === "-") {
+    return firstOperand - secondOperand;
+  } else if (operator === "*") {
+    return firstOperand * secondOperand;
+  } else if (operator === "/") {
+    return firstOperand / secondOperand;
+  }
+
+  return secondOperand;
+}
+
+function resetCalculator() {
+  calculator.displayValue = "0";
+  calculator.firstOperand = null;
+  calculator.waitingForSecondOperand = false;
+  calculator.operator = null;
+  console.log(calculator);
+}
+function updateDisplay() {
+  const display = document.querySelector(".calculator-screen");
+  display.value = calculator.displayValue;
+}
+
+updateDisplay();
+// updateDisplay();
+
+const keys = document.querySelector(".calculator-keys");
+keys.addEventListener("click", (event) => {
+  const { target } = event;
+  const { value } = target;
+  if (!target.matches("button")) {
+    return;
+  }
+
+  switch (value) {
+    case "+":
+    case "-":
+    case "*":
+    case "/":
+    case "=":
+      handleOperator(value);
+      break;
+    case ".":
+      inputDecimal(value);
+      break;
+    case "all-clear":
+      resetCalculator();
+      break;
+    default:
+      // check if the key is an integer
+      if (Number.isInteger(parseFloat(value))) {
+        inputDigit(value);
+      }
+  }
+
+  updateDisplay();
 });
-
-const numberButtons = document.querySelectorAll("[data-number]");
-
-numberButtons.forEach((button) => {
-  button.addEventListener("click", () => {
-    console.log(button.innerText);
-  });
-});
-
-// const previousOperandTextElement = document.querySelectorAll(
-//   "[data-previous-operand]"
-// );
-// const currentOperandTextElement = document.querySelectorAll(
-//   "[data-current-operand]"
-// );
-
-// this.currentOperandTextElement
